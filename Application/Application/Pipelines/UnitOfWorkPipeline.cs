5e57d07215bd28734712ca8e7aaf227b7356860b
Shamil Sadigov <shamil.sadigov@indeed-id.com>
2021-08-17 04:56:22 +0400

    Add application pipelines

---
+++ Application/Application/Pipelines/UnitOfWorkPipeline.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Contracts;
+using MediatR;
+
+namespace Application.Pipelines
+{
+    public class UnitOfWorkPipeline<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest: notnull
+    {
+        private readonly IDomainEventsPublisher _domainEventsPublisher;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UnitOfWorkPipeline(IDomainEventsPublisher domainEventsPublisher, IUnitOfWork unitOfWork)
+        {
+            _domainEventsPublisher = domainEventsPublisher;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<TResponse> Handle(
+            TRequest request,
+            CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> nextHandler)
+        {
+            var response = await nextHandler();
+            
+            await _domainEventsPublisher.PublishEventsAsync();
+
+            await _unitOfWork.CommitAsync();
+
+            return response;
+        }
+    }
+}
---
+++ Application/Application/Pipelines/ValidationPipeline.cs
@@ -0,0 +1,46 @@
+﻿#region
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+#endregion
+
+namespace Application.Pipelines
+{
+    public class ValidationPipeline<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest: notnull
+    {
+        private readonly ILogger<ValidationPipeline<TRequest, TResponse>> _logger;
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationPipeline(
+            IEnumerable<IValidator<TRequest>> validators,
+            ILogger<ValidationPipeline<TRequest, TResponse>> logger)
+        {
+            _validators = validators;
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(
+            TRequest request,
+            CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
+        {
+            var failures = _validators
+                .Select(v => v.Validate(request))
+                .SelectMany(result => result.Errors)
+                .Where(error => error != null)
+                .ToList();
+
+            if (failures.Any())
+                throw new ValidationException(failures);
+
+            return await next();
+        }
+    }
+}
