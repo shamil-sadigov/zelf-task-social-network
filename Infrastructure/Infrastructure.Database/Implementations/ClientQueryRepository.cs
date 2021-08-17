b5bd17717a71aea8ead4963f12bb481e7805264d
Shamil Sadigov <shamil.sadigov@indeed-id.com>
2021-08-17 13:35:23 +0400

    Fix sqlLite mapping bugs

--- Infrastructure/Infrastructure.Database/Implementations/ClientQueryRepository.cs
+++ Infrastructure/Infrastructure.Database/Implementations/ClientQueryRepository.cs
@@ -6,6 +6,9 @@ using Application.Contracts;
 using Application.Queries;
 using Application.Queries.GetTopPopularClients;
 using Dapper;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using MoreLinq;
 
 namespace Infrastructure.Database.Implementations
 {
@@ -14,37 +17,56 @@ namespace Infrastructure.Database.Implementations
         private readonly ISqlConnectionFactory _sqlConnectionFactory;
 
         public ClientQueryRepository(ISqlConnectionFactory sqlConnectionFactory)
-            => _sqlConnectionFactory = sqlConnectionFactory;
+        {
+            _sqlConnectionFactory = sqlConnectionFactory;
+        }
 
         public async Task<List<ClientDto>> GetTopPopularAsync(ushort limit)
         {
             var connection = _sqlConnectionFactory.GetOrCreateConnection();
-
-            var clients  = await connection.QueryAsync<ClientDto>(
-                "SELECT Id, Name, Popularity"+
-                "FROM clients" +
-                "ORDER BY Popularity" +
+            
+            var clients  = await connection.QueryAsync<ClientDbModel>(
+                "SELECT [Id], [Name], [Popularity]"+
+                "FROM Clients" +
+                "ORDER BY [Popularity]" +
                 "LIMIT @Limit" , new
                 {
                     Limit = limit,
                 });
 
-            return clients.ToList();
+             return clients
+                 .Select(x=> MapToDto(x))
+                 .ToList();
         }
 
         public async Task<ClientDto?> GetByIdAsync(Guid clientId)
         {
             var connection = _sqlConnectionFactory.GetOrCreateConnection();
-
-            var result  = await connection.QuerySingleOrDefaultAsync<ClientDto>(
-                "SELECT Id, Name, Popularity"+
-                "FROM clients" +
-                "WHERE Id=@ClientId", new
+            
+            var result  = await connection.QuerySingleOrDefaultAsync<ClientDbModel>(
+                "SELECT [Id], [Name], [Popularity]"+
+                "FROM Clients " +
+                "WHERE [Id]=@ClientId", new
                 {
-                    ClientId = clientId,
+                    ClientId = clientId.ToString().ToUpper(),
                 });
 
-            return result;
+            return result is null ? null : MapToDto(result);
         }
+        
+        private class ClientDbModel
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public ushort Popularity { get; set; }
+        };
+
+        private static ClientDto MapToDto(ClientDbModel model)
+             => new
+             (
+                Guid.Parse(model.Id),
+                model.Name,
+                model.Popularity
+             );
     }
 }
